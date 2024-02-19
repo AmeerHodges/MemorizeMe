a7a3690ae30d0f96af747958ec10b5828ff1181f
Ameer Hodges <hodgesameer@gmail.com>
2024-02-19 20:11:06 +0000

    added a object oriented homepage to navigation Stack in App.js

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Components/Home";
 
-export default function App() {
+//Dont touch this constant, its how im working on navigation
+const Stack = createNativeStackNavigator();
+
+//what loads up when the page starts
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+// All CSS goes here, Same format As CSS if you know it
+const styles = StyleSheet.create({});
