27ae8b14cf2a15e3ae5a1afc053a4af17f3897de
21TOM21 <zommyy0731@gmail.com>
2024-03-08 16:29:17 +0000

    Back button
    Back button is added on the top left

--- Components/Subject.js
+++ Components/Subject.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Feather from "react-native-vector-icons/Feather";
+import colors from "../assets/Colors/Colors.js";
 
 export default Subject = ({ route }) => {
   const { item } = route.params;
+  const navigation = useNavigation();
+
   console.log({ item });
 
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <SafeAreaView>
         <View style={styles.headerWrapper}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+            <Feather name="chevron-left" size={24} color={colors.textDark} />
+          </TouchableOpacity>
           <View style={styles.headerLeft}>
             <Text>{item.title}</Text>
           </View>
@@ -18,8 +26,22 @@ export default Subject = ({ route }) => {
   );
 };
 
-const styles = new StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    alignItems: "center",
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+  },
+  backButton: {
+    marginRight: 10,
   },
 });
