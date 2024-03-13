e5d48e358b80096b2270ff4a678d3cb24fc132ed
Ameer Hodges <hodgesameer@gmail.com>
2024-03-13 14:15:22 +0000

    made a login page, will be using it to try to     get a database to work

---
+++ Components/Login.js
@@ -0,0 +1,6 @@
+import { React, useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as SQLite from "expo-sqlite";
+
+export default Login = () => {};
